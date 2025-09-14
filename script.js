693bc3ecb29a485ae9a3af7b274a34dbc0f8395d
John <18509053+frogsforjp@users.noreply.github.com>
2025-09-14 18:20:31 -0400

    feat: Add operate function for calculations

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // operatorState defaults to none on start and clear()
 // states are add, subtract, multiply, divide, none
-const operatorState = 'none';
-const display = 0;
+let operatorState = 'none';
+let display = 0;
 
 function add(numA, numB) {
     return numA + numB;
@@ -19,28 +19,33 @@ function divide(numA, numB) {
     return numB === 0 ? 'undefined' : numA / numB;
 }
 
-// operate:
-// check operatorState (+, -, *, or /)
-
-// switch (operatorState):
-//     CASES
-//     add
-//     subtract
-//     multiply
-//     divide
-//     none: (do nothing. just keep the same display)
-
-
-
+function operate(numA, numB) {
+    switch (operatorState) {
+        case 'add':
+            display = add(numA, numB);
+            return display;
+        case 'subtract':
+            display = subtract(numA, numB);
+            return display;
+        case 'multiply':
+            display = multiply(numA, numB);
+            return display;
+        case 'divide':
+            display = divide(numA, numB);
+            return display;
+        case 'none':
+            // the display shouldn't change when you hit = without an operator
+            return;
+        default:
+            return 'Error';
+    }
+}
 
 // updateDisplay()? (or alternatively just an event listener on each button with an anonymous function)
 //     the operators will update the state but not display
 //     only numbers will be added to display
 
-
 //  (don't clear last operation. allow user to spam = to repeat last operation)
-
-
 
 
 
